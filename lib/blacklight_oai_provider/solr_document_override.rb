3179775b156382703fc08979235dc50e92621635
Chris Beer <chris_beer@wgbh.org>
2010-10-28 13:32:33 -0400

    use solrdocument semantic fields to map to dc

--- lib/blacklight_oai_provider/solr_document_override.rb
+++ lib/blacklight_oai_provider/solr_document_override.rb
@@ -11,6 +11,19 @@ module BlacklightOaiProvider::SolrDocumentOverride
     export_as('oai_dc_xml')
   end
   def export_as_oai_dc_xml
-    '' # tk
+    xml = Builder::XmlMarkup.new
+    xml.tag!("oai_dc:dc",
+             'xmlns:oai_dc' => "http://www.openarchives.org/OAI/2.0/oai_dc/",
+             'xmlns:dc' => "http://purl.org/dc/elements/1.1/",
+             'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
+             'xsi:schemaLocation' => %{http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd}) do
+       self.to_semantic_values.each do |field,values|
+         values.each do |v|
+           xml.tag! 'dc:' + field.to_s, v
+         end
+       end
+     end
+
+     xml.target!
   end
 end
