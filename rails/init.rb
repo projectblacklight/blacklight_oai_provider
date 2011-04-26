8c4b12b8f1801b793ab220b2d3f5e61347158299
Chris Beer <chris_beer@wgbh.org>
2011-04-26 14:11:51 -0400

    rails 3 lazy update

--- rails/init.rb
+++ rails/init.rb
@@ -4,6 +4,6 @@ require 'blacklight_oai_provider'
 # it in an initializer, using BlacklightOaiProvider.omit_inject .
 # Only weirdness about this is our CSS will always be last, so if an app
 # wants to over-ride it, might want to set BlacklightOaiProvider.omit_inject => {:css => true}
-config.after_initialize do 
+config.to_prepare do
   BlacklightOaiProvider.inject!
 end
