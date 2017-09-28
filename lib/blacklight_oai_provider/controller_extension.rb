db49aa694a31daac728075035c61acdb6086b549
Carla Galarza <cmg2228@columbia.edu>
2017-09-28 12:52:21 -0400

    Namespacing XSL stylesheet in assets directory.

--- lib/blacklight_oai_provider/controller_extension.rb
+++ lib/blacklight_oai_provider/controller_extension.rb
@@ -1,6 +1,6 @@
 # Meant to be applied on top of a controller that implements
 # Blacklight::SolrHelper. Will inject range limiting behaviors
-# to solr parameters creation. 
+# to solr parameters creation.
 module BlacklightOaiProvider::ControllerExtension
   def self.included(some_class)
     some_class.helper_method :oai_config
@@ -9,10 +9,10 @@ module BlacklightOaiProvider::ControllerExtension
   # Action method of our own!
   # Delivers a _partial_ that's a display of a single fields range facets.
   # Used when we need a second Solr query to get range facets, after the
-  # first found min/max from result set. 
+  # first found min/max from result set.
   def oai
     options = params.delete_if { |k,v| %w{controller action}.include?(k) }
-    render :text => oai_provider.process_request(options).gsub('<?xml version="1.0" encoding="UTF-8"?>', "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<?xml-stylesheet type=\"text/xsl\" href=\"#{ ActionController::Base.helpers.asset_path('oai2.xsl')}\" ?>"), :content_type => 'text/xml'
+    render :text => oai_provider.process_request(options).gsub('<?xml version="1.0" encoding="UTF-8"?>', "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<?xml-stylesheet type=\"text/xsl\" href=\"#{ ActionController::Base.helpers.asset_path('blacklight_oai_provider/oai2.xsl')}\" ?>"), :content_type => 'text/xml'
   end
 
   # Uses Blacklight.config, needs to be modified when
