96172f9f977871937139a1588becde16584254b5
Carla Galarza <cmg2228@columbia.edu>
2017-09-28 12:53:23 -0400

    Adding XSL stylesheet to list of assets to be precompiled by default.

--- lib/blacklight_oai_provider/engine.rb
+++ lib/blacklight_oai_provider/engine.rb
@@ -4,15 +4,21 @@ require 'rails'
 
 module BlacklightOaiProvider
   class Engine < Rails::Engine
-  
+
     # Do these things in a to_prepare block, to try and make them work
     # in development mode with class-reloading. The trick is we can't
     # be sure if the controllers we're modifying are being reloaded in
     # dev mode, if they are in the BL plugin and haven't been copied to
-    # local, they won't be. But we do our best. 
+    # local, they won't be. But we do our best.
     config.to_prepare do
       BlacklightOaiProvider.inject!
     end
+
+    # Add XSL Stylesheet to list of assets to be precompiled.
+    initializer "blacklight_oai_provider.assets.precompile" do |app|
+      app.config.assets.precompile += %w( blacklight_oai_provider/oai2.xsl )
+    end
+
     # Load rake tasks.
     rake_tasks do
       Dir.chdir(File.expand_path(File.join(File.dirname(__FILE__), '..'))) do
