51b3554e81670912b4742d0a1c59e02accf97c41
Carla Galarza <cmg2228@columbia.edu>
2017-10-02 10:52:33 -0400

    Adding more configuration to generated test application.   - Semantic field hash in BL needs to be updated.   - Returning all fields with results.   - Adding oai blacklight configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,24 +6,6 @@ EngineCart.load_application!
 require 'rspec/rails'
 require 'capybara/rspec'
 
-#
-# # Setup blacklight environment
-# Blacklight.solr_config = { :url => 'http://127.0.0.1:8983/solr' }
-# CatalogController.send(:include, BlacklightOaiProvider::ControllerExtension)
-#
-# class SolrDocument
-#   include Blacklight::Solr::Document
-#   include BlacklightOaiProvider::SolrDocumentExtension
-#   use_extension( Blacklight::Solr::Document::DublinCore)
-#   field_semantics.merge!(
-#                          :title => "title_display",
-#                          :author => "author_display",
-#                          :language => "language_facet",
-#                          :format => "format"
-#                          )
-#
-# end
-
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 end
--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -1,7 +1,6 @@
 require 'rails/generators'
 
 class TestAppGenerator < Rails::Generators::Base
-  # source_root "./spec/test_app_templates"
   source_root File.expand_path("../../../../spec/test_app_templates", __FILE__)
 
   # if you need to generate any additional configuration
@@ -25,7 +24,6 @@ class TestAppGenerator < Rails::Generators::Base
     end
   end
 
-
   # Override solr.yml to match settings needed for solr_wrapper.
   def update_solr_config
     say_status("warning", "COPYING SOLR.YML", :yellow)
@@ -38,5 +36,38 @@ class TestAppGenerator < Rails::Generators::Base
     say_status("warning", "GENERATING BL OAI PLUGIN", :yellow)
 
     generate 'blacklight_oai_provider:install'
+  end
+
+  def add_test_blacklight_oai_config
+    say_status("warning", "ADDING BL OIA-PMH CONFIG")
+
+    insert_into_file "app/controllers/catalog_controller.rb", :after => "    config.default_solr_params = { \n" do
+      "      :fl => '*',\n"
+    end
+
+    insert_into_file "app/controllers/catalog_controller.rb", :after => "configure_blacklight do |config|\n" do
+%{
+    config.oai = {
+        :provider => {
+        :repository_name => 'Test',
+        :repository_url => 'http://localhost',
+        :record_prefix => '',
+        :admin_email => 'root@localhost'
+      },
+      :document => {
+        :timestamp => 'timestamp',
+        :limit => 25
+      }
+    }
+}
+    end
+
+    insert_into_file "app/models/solr_document.rb", :after => "  field_semantics.merge!(    \n" do
+%{
+    :creator => "author_display",
+    :date => "pub_date",
+    :subject => "subject_topic_facet",
+}
+    end
   end
 end
