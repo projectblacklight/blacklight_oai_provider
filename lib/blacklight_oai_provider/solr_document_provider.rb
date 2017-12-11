0193451597ac55d4cfbaf720befd18725c5bda62
Carla Galarza <cmg2228@columbia.edu>
2017-12-11 15:19:34 -0500

    Updating url method for oai.   - Url helper got renamed from oai_provider_path to oai_catalog_path

--- lib/blacklight_oai_provider/solr_document_provider.rb
+++ lib/blacklight_oai_provider/solr_document_provider.rb
@@ -9,7 +9,7 @@ module BlacklightOaiProvider
       self.class.model = SolrDocumentWrapper.new(controller, options[:document])
 
       options[:repository_name] ||= controller.view_context.send(:application_name)
-      options[:repository_url] ||= controller.view_context.send(:oai_provider_url)
+      options[:repository_url] ||= controller.view_context.send(:oai_catalog_url)
 
       options[:provider].each do |k, v|
         self.class.send k, v
