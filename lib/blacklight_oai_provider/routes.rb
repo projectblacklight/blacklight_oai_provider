eaf1cb351e7ff9fb5dc533780c3a8e04bf0eae4d
Carla Galarza <cmg2228@columbia.edu>
2017-12-11 15:16:47 -0500

    Adding new routing concerns for blacklight 6. Removed inject! method because its now optional to add the routing concern.

--- lib/blacklight_oai_provider/routes.rb
+++ lib/blacklight_oai_provider/routes.rb
@@ -1,15 +1,11 @@
 module BlacklightOaiProvider
-  module Routes
-    extend ActiveSupport::Concern
-
-    included do |klass|
-      klass.default_route_sets.insert(klass.default_route_sets.index(:export), :oai_routing)
+  class Routes
+    def initialize(defaults = {})
+      @defaults = defaults
     end
 
-    def oai_routing(primary_resource)
-      add_routes do
-        get "#{primary_resource}/oai", to: "#{primary_resource}#oai", as: 'oai_provider'
-      end
+    def call(mapper, _options = {})
+      mapper.match 'oai', action: 'oai', via: [:get, :post]
     end
   end
 end
