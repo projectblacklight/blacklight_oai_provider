44b4dce4aa47baccd81e746ede3d412dd4873701
Eneko Taberna <eneko.taberna@kashing.co.uk>
2020-12-15 10:51:21 +0100

    Fixes Nil Pointer Exception case when parsing the dates of the catalog and no work was returned

--- lib/blacklight_oai_provider/solr_document_wrapper.rb
+++ lib/blacklight_oai_provider/solr_document_wrapper.rb
@@ -25,13 +25,13 @@ module BlacklightOaiProvider
     def earliest
       builder = search_service.search_builder.merge(fl: solr_timestamp, sort: "#{solr_timestamp} asc", rows: 1)
       response = search_service.repository.search(builder)
-      response.documents.first.timestamp
+      response.documents.first&.timestamp || Time.now
     end
 
     def latest
       builder = search_service.search_builder.merge(fl: solr_timestamp, sort: "#{solr_timestamp} desc", rows: 1)
       response = search_service.repository.search(builder)
-      response.documents.first.timestamp
+      response.documents.first&.timestamp || Time.now
     end
 
     def find(selector, options = {})
